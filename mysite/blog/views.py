df52e88b31343a5e5ca358e92062d5d23d081784
alfonso <alfareiza@gmail.com>
2021-11-11 18:57:58 -0300

    Compartilhamento de post by e-mail
    exemplo de .env
    
    Variáveis de ambiente para configuração de e-mail
    
    view para compartilhar post
    
    url para compartilhar post
    
    formulário para compartilhamento de post
    
    html que renderiza o formulário para compartilhar post
    
    Adicionado link para compartilhamento de post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from mysite.blog.forms import EmailPostForm
 from mysite.blog.models import Post
 
 
@@ -34,3 +36,22 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Obtém a postagem com base no id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data  # cd recebe um dict com os valores obtidos
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} te recomenda ler {post.title}"
+            message = f"Te recomendo que leia o post \'{post.title}\' em {post_url}\n\nComentários de {cd['name']}: {cd['comments']}"
+            send_mail(subject, message, 'alfareiza@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-trdgdfxq3jq50w#&8m)2s!yqe-^a0_19-^_^h43u9#fq%tbadn'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -124,3 +125,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration
+
+# E-Mail Configuration
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True)
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', default=587)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
