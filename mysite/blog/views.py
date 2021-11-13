52659ce710f31c03aa1d2821859ab34aff85c42e
alfonso <alfareiza@gmail.com>
2021-11-13 18:51:25 -0500

    Capturando no banco a lista de post similares

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from taggit.models import Tag
@@ -57,9 +58,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # Pag. 87
+    # Lista de id de tags do post atual. Ex.: <QuerySet [3, 5]>
+    post_tags_ids = post.tags.values_list('id', flat=True)
+
+    # Lista de postagens semelhantes Ex. :<QuerySet [<Post:  __str__ do model>, <Post: __str__ do model >]>
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+
+    # Lista dos 4 primeiros objetos tipo posts baseado no atributo criado -same_tags-
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                   {'post': post, 'comments': comments,
-                   'new_comment': new_comment, 'comment_form': comment_form})
+                   'new_comment': new_comment, 'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
