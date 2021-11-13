1b7cfb9e110f225a1e75e28173b06eace282e760
alfonso <alfareiza@gmail.com>
2021-11-13 15:26:34 -0500

    Feature: tags nos posts
    Modelagem para tags
    
    Mostrando tags do post
    
    Mostrando tags por post
    
    URL para exibir post por tags
    
    Tratamento para aceitar tags e filtrar caso seja requesitado
    
    app taggit adicionado
    
    lib django-taggit instalada

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,8 +5,14 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    # Listar todos os posts
+    path('', views.post_list, name='post_list'),
+    # Listar posts baseado num tag
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    # Detalhe de uma view
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    # Compartilhar view
     path('<int:post_id>/share/', views.post_share, name='post_share')
+
 ]
