ae20135854c871a8723bc510d532ddb6c7d0b81e
Alfonso <alfareiza@gmail.com>
2021-11-26 14:52:00 -0500

    URl sitemap.xml adicionada, apontando para view sitemap

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+
+from mysite.blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.blog.urls', namespace='blog')),
+    # Pág 103
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
